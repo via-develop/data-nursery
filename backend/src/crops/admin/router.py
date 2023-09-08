a50c28beb2621bdc14edd20543532e2f8167340b
viaDPBell <minjong@viasofts.com>
2023-09-08 13:41:56 +0900

    [Feat] - 작물 수정 api 완료

--- backend/src/crops/admin/router.py
+++ backend/src/crops/admin/router.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, UploadFile, File, Form, Request
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
+
+import os
 
 from utils.database import get_db
-from utils.db_shortcuts import get_current_user
+from utils.db_shortcuts import get_current_user, get_
+from utils.file_upload import single_file_uploader, delete_file
 import src.crops.models as cropModels
+from settings import BASE_DIR
 
 
 router = APIRouter()
@@ -27,3 +32,38 @@ def search_crop_name_for_admin(
     crop_name_response = [result[0] for result in crop_name_query]
 
     return crop_name_response
+
+
+@router.patch("/update/{crop_id}", description="관리자 작물 정보 업데이트 api", status_code=200)
+async def update_admin_crop_info(
+    request: Request,
+    crop_id: int,
+    name: str = Form(...),
+    color: str = Form(...),
+    image: UploadFile = File(...),
+    db: Session = Depends(get_db),
+):
+    get_current_user("99", request.cookies, db)
+
+    crop = get_(db, cropModels.Crop, id=crop_id)
+
+    if not crop:
+        return JSONResponse(status_code=404, content=dict(msg="NOT_FOUND_CROP"))
+
+    old_image = crop.image
+
+    saved_file = await single_file_uploader(image)
+
+    if not saved_file["is_success"]:
+        return JSONResponse(status_code=400, content=dict(msg="FAIL_SAVE_DATA"))
+
+    crop.name = name
+    crop.color = color
+    crop.image = saved_file["url"]
+
+    db.commit()
+    db.refresh(crop)
+
+    await delete_file(old_image)
+
+    return JSONResponse(status_code=201, content=dict(msg="CROP_CREATE_SUCCESS"))
